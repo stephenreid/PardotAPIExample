5286b530357f60649b06673e553114512e298cca
Stephen Reid <stephenreid@ratedsar.com>
2011-07-13 18:09:42 -0400

    Add some more functions

--- PardotConnector.class.php
+++ PardotConnector.class.php
@@ -3,14 +3,51 @@ class PardotConnector
 {
 	//A flag for echoing debug output
 	private $debug = false;
+	private $userKey;
+	private $apiKey;
 
 	public function __construct()
 	{
 	}
-	public function authenticate(){
+	public function authenticate($username,$password,$userKey){
 		//gets a user api key back
-		return $key;
+		$this->userKey= $userKey;
+		$ret = $this->send('login','',array($username,$password,$userKey));
+		$this->apiKey=$ret->api_key;//add error handling to this later
+		return $ret;
 	}
+	/**
+	 * 
+	 * Creates a prospect in Pardot
+	 * @param unknown_type $arr
+	 * $arr contaisn an array of fields
+	 * Requieres at least email=>test@test.com
+	 * see for more : http://developer.pardot.com/kb/api-version-3/using-prospects
+	 */
+	public function createProspect($arr){
+		$ret = $this->send('prospect','create',$arr);
+		return $ret;
+	}
+	public function getProspectById($id){
+		$ret = $this->send('prospect','read',array('id'=>$id));
+		return $ret;
+	}
+	public function getProspectById($email){
+		$ret = $this->send('prospect','read',array('email'=>$email));
+		return $ret;
+	}
+	public function updateProspect($identifier,$arr){
+		//is this an email or an id
+		if (strpos($identifier,'@')){
+			//this is an email address
+		} // else it's an id
+		$ret = $this->send('prospect','update',array('email'=>$email));
+		
+		
+		
+	}
+
+	public function 
 	/**
 	 * Send
 	 * @desc Sends a web request to the api
@@ -26,6 +63,7 @@ class PardotConnector
 				'method'	=> 'POST',
 				'header'	=> 'Content-type: application/x-www-form-urlencoded',
 				'content'	=> http_build_query($parameters),
+				//add a timeout here
 			)	
 		));
 		$res = file_get_contents($url,false,$context);
