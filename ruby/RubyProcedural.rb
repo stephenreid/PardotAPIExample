ed076be6ec7b2572df1586751891814ebe0a3e8a
Stephen Reid <stephenreid@ratedsar.com>
2011-08-15 15:55:05 -0400

    fodlerizing did not work

--- ruby/RubyProcedural.rb
+++
@@ -1,8 +0,0 @@
-def authenticate
-  require 'net/http'
-  url = URI.parse('https://pi.pardot.com/api/login/version/3')
-  # build the params string
-  post_args1 = { 'username' => params[:username], 'user_key' => params[:user_key] , 'password' => params[:password]  }
-  # send the request
-  resp, data = Net::HTTP.post_form(url, post_args1)
-end
