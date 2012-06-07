4967d83b419b12adccfd00c9f8c51a0295fd4688
Stephen Reid <stephen.reid@ratedsar.com>
2012-06-06 20:59:56 -0400

    Update our simple test with new methods and tests

--- test.php
+++ test.php
@@ -5,14 +5,21 @@
  * Then we can use magical getters and setters
  * to update the data array.
  * This should work on default and custom fields.
- * LAST TESTED 6/5/2012
+ * LAST TESTED 6/6/2012
  */
 include('./PardotConnector.class.php');
 include('./Prospect.class.php');
 $p = new Prospect();
-$p = $p->fetchProspectByEmail('api@pardot.com');
-$p->email = 'api+10@pardot.com';
+$p = $p->fetchProspectByEmail('test@test.com');
+$p->email = 'test+11@test.com';
 $p->save();
-$p->email = 'api@pardot.com';
-$p-save();
+$p->email = 'test@test.com';
+$p->save();
 
+
+
+$connector = new PardotConnector();
+$connector->authenticate();
+var_dump($connector->accountRead());
+var_dump($connector->campaignQuery());
+var_dump($connector->formQuery());
