787904d561df0c641b2d6675b676b5a1330c51c1
Stephen Reid <stephen.reid@ratedsar.com>
2012-08-30 13:42:49 -0400

    #2 Remove placeholder values for email and password. These should really only be set by the authenticate method.(Storage is debateable but could be useful for renewing a connection (catch api key expired))

--- PardotConnector.class.php
+++ PardotConnector.class.php
@@ -14,9 +14,9 @@ class PardotConnector
 	private $apiKey		= null;
 	private $outputMode	= 'full'; // choose between 'simple','full','mobile'
 
-	private $email = '####';
-	private $password = '####';
-	private $userKey = '####';
+	private $email = '';
+	private $password = '';
+	private $userKey = '';
 
 
 	/**
